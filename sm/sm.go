0fd4139f174192e786e138ec65820c3873621813
kevin <651312867@qq.com>
2017-02-03 18:30:09 +0800

    aa

--- sm/sm.go
+++ sm/sm.go
@@ -3,7 +3,9 @@ package sm
 import (
 	"log"
 	"strings"
-	"time"
+	// "time"
+	"fmt"
+	"strconv"
 )
 
 //定义列结构体
@@ -32,11 +34,47 @@ const (
 	TypeDate
 )
 
+//定义语法错误结构体
+type grammerError struct {
+	msg string
+}
+
+func (ge grammerError) Error() string {
+	return fmt.Sprintf("语法错误--%s", ge.msg)
+}
+
 //根据ddl语句生成表结构体
-func GenTableByDdl(sql string) (table, error) {
-	//sql语言校验
+func GenTableByDdl(sql string) (*table, error) {
+	//todo sql语言校验
+	words := SqlToWords(sql)
 	genTable := new(table)
-	genTable.name = ""
+	genTable.name = words[2]
+	indexs1 := index(words, "(")
+	indexs2 := index(words, ",")
+	indexs3 := index(words, ")")
+	log.Println(indexs1)
+	log.Println(indexs2)
+	log.Println(indexs3)
+	slice := make([]int, 0)
+	slice = append(slice, indexs1[0])
+	slice = append(slice, indexs2...)
+	slice = append(slice, indexs3[len(indexs3)-1])
+	log.Println(slice)
+	colNumber := len(slice) - 1 //列数量
+	columns := make([]column, colNumber)
+	for i := 0; i < colNumber; i++ {
+		startIndex := slice[i]
+		endIndex := slice[i+1]
+		colWords := words[startIndex+1 : endIndex]
+		log.Println(colWords)
+		col, err := genColumn(colWords)
+		if err != nil {
+			return genTable, err
+		}
+		columns[i] = col
+	}
+	log.Println(columns)
+	genTable.columns = columns
 	return genTable, nil
 }
 
@@ -45,7 +83,7 @@ func GenTableByDdl(sql string) (table, error) {
 //根据frm文件生成表结构体
 
 //把sql语句转换为一个一个单词
-func SqlToWords(sql string) (words []string, err error) {
+func SqlToWords(sql string) (words []string) {
 	// runes := ([]rune)sql
 	chars := strings.Split(sql, "")
 	specialChars := "(),;"
@@ -62,5 +100,57 @@ func SqlToWords(sql string) (words []string, err error) {
 			word = word + char
 		}
 	}
+	words = append(words, word)
+	log.Printf("%v,%d\n", words, len(words))
 	return
+}
+
+func index(words []string, word string) (indexs []int) {
+	return index0(words, word, false)
+}
+
+func index0(words []string, word string, isIgnoreCase bool) (indexs []int) {
+	if isIgnoreCase {
+		for n, w := range words {
+			if strings.ToLower(w) == strings.ToLower(word) {
+				indexs = append(indexs, n)
+			}
+		}
+	} else {
+		for n, w := range words {
+			if w == word {
+				indexs = append(indexs, n)
+			}
+		}
+	}
+	return
+}
+
+//根据ddl词切片生成column
+func genColumn(words []string) (column, error) {
+	col := new(column)
+	col.name = words[0]
+	switch strings.ToLower(words[1]) {
+	case "number":
+		col.dataType = TypeInt
+	case "varchar2":
+		col.dataType = TypeString
+	default:
+		return *col, grammerError{"不支持字段:" + col.name + "的字段类型:" + words[1]}
+	}
+	width, err := strconv.ParseInt(words[3], 0, 64)
+	if err != nil {
+		return *col, err
+	}
+	col.dataWidth = int(width)
+	indexs := index0(words, "NULL", true)
+	switch len(indexs) {
+	case 0:
+		col.isNull = true
+	case 1:
+		col.isNull = false
+	default:
+		return *col, grammerError{"字段" + col.name + "格式有误"}
+	}
+	return *col, nil
 }
--- sm/sm_test.go
+++ sm/sm_test.go
@@ -1,19 +1,33 @@
 package sm
 
 import (
-	// "kqdb/sm"
+	"kqdb/sm"
+	"strings"
 	"testing"
 )
 
-type teststruc struct {
-	tt string
-	aa int
+func test_test(t *testing.T) {
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	num1 := numbers[0:5]
+	num1[2] = 33333
+	t.Log(num1)
+	t.Log(numbers)
+	t.Log(strings.ToLower("(),"))
 }
 
-func Test_test(t *testing.T) {
-	// var i int = 32
-	words := make([]string, 2)
-	words = append(words, "hello")
-	t.Logf("%v", words)
-	t.Log(words)
+func test_SqlToWords(t *testing.T) {
+	sql := "CREATE TABLE region(" +
+		"ID number(2) NOT NULL PRIMARY KEY," +
+		"postcode number(6) default '0' NOT NULL," +
+		"areaname varchar2(30) default ' ' NOT NULL);"
+	sm.SqlToWords(sql)
+}
+
+func Test_GenTableByDdl(t *testing.T) {
+	sql := "CREATE TABLE region(" +
+		"ID number(2) NOT nULL PRIMARY KEY," +
+		"postcode number(6) default '0' NOT NULL," +
+		"areaname varchar2(30) default ' ' NOT NULL);"
+	_, err := sm.GenTableByDdl(sql)
+	t.Log(err)
 }
